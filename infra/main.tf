a64754bae9576be1afa45a838b535aaa6b41ca35
Axel Antunes <105727674+Flamware@users.noreply.github.com>
2026-03-11 21:32:57 +0100

    feat(infra): Provisions K3s cluster node and necessary firewall rules

--- infra/main.tf
+++ infra/main.tf
@@ -21,3 +21,52 @@ resource "google_compute_subnetwork" "public_subnet" {
   region = var.region
   network = google_compute_network.vpc_network.id
 }
+
+# Le pare-feu pour autoriser le trafic entrant sur les ports 80 (HTTP) et 22 (SSH)
+resource "google_compute_firewall" "allow_http_ssh" {
+  name    = "allow-http-ssh"
+  network = google_compute_network.vpc_network.id
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "22", "6443"]
+  }
+
+  target_tags = ["allow-http-ssh"]
+  source_ranges = ["0.0.0.0/0"] # TODO: restreindre à des plages IP spécifiques pour plus de sécurité
+}
+
+resource "google_compute_instance" "k3s" {
+  name = "k3s-vm-1"
+  machine_type = "e2-micro"
+  zone = var.zone
+
+  # On attache le tag pour que le pare-feu puisse appliquer les règles
+  tags = ["allow-http-ssh"]
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size = 10 # Taille du disque en GB
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.id
+    subnetwork = google_compute_subnetwork.public_subnet.id
+    access_config {
+      # Permet d'obtenir une adresse IP publique
+    }
+  }
+
+  # Script d'initialisation pour installer k3s
+    metadata_startup_script = <<-EOT
+        #!/bin/bash
+        curl -sfL https://get.k3s.io | sh -
+    EOT
+}
+
+output "instance_ip" {
+  description = "The public IP address of the k3s VM instance."
+  value = ""
+}
